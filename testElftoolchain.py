e27a0a0502b8f2927a145bae6b101a2f50d26c26
tigergao99 <tigergao1999@outlook.com>
2020-12-23 11:16:16 -0500

    Added test result comparison

--- testElftoolchain.py
+++ testElftoolchain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import sys
 
 filelist = {
     'llvm-ar', 'llvm-addr2line', 'llvm-nm', 'llvm-objcopy', 'llvm-objdump',
@@ -21,19 +22,57 @@ def run_tests():
     for f in filelist:
         # llvm-strip tested in llvm-objcopy and llvm-objdump
         if f != 'llvm-strip':
+            print(f, flush=True)
             if f in test_dir:
-                os.system(f'./llvm-lit ../../llvm/test/tools/{test_dir[f]}')
+                os.system(f'./llvm-lit -q ../../llvm/test/tools/{test_dir[f]}')
             else:
-                os.system(f'./llvm-lit ../../llvm/test/tools/{f}')
+                os.system(f'./llvm-lit -q ../../llvm/test/tools/{f}')
+
+def parse_testsuite(file):
+    prog = None
+    failed_tests = set()
+    for line in file:
+        line = line.strip()
+        if line in filelist:
+            if prog:
+                yield (prog, failed_tests)
+            prog = line
+        elif line[:4] == 'LLVM':
+            failed_tests.add(line.split()[2])
+
+def compare_test_results(file1, file2):
+    for (p1, failed1), (p2, failed2) in zip(parse_testsuite(file1), parse_testsuite(file2)):
+        try:
+            if p1 != p2:
+                raise ValueError('Two test results are not run in same order or one is missing some elftoolchain executables.')
+        except:
+            sys.exit(1)
+        print(p1)
+        print(f'Additional tests that passed in {file2.name} since {file1.name}:')
+        for test in failed1 - failed2:
+            print('\t' + test)
+        print(f'Tests that regressed in {file2.name} since {file1.name}:')
+        for test in failed2 - failed1:
+            print('\t' + test)
 
 def main():
     parser = argparse.ArgumentParser(prog='elftoolchain_tester', description='Test elftoolchain against LLVM tests.')
-    parser.add_argument('-p', '--path', required=True, help='Path of llvm-project checkout.')
+    parser.add_argument('-p', '--path', help='Path of llvm-project checkout.')
+    parser.add_argument('file', type=argparse.FileType('r'), nargs='*')
     args = parser.parse_args()
     
-    os.chdir(args.path + '/build/bin')
-    create_symlinks()
-    run_tests()
+    # comparing two test run results
+    if len(args.file) == 2:
+        compare_test_results(*args.file)
+    # testing elftoolchain
+    elif len(args.file) == 0:
+        if not args.path:
+            raise parser.error('Please specify llvm-project checkout via -p or --path.')
+        os.chdir(args.path + '/build/bin')
+        create_symlinks()
+        run_tests()
+    else:
+        raise parser.error('Either pass 2 files for comparison or no files to run a new test.')
 
 if __name__ == "__main__":
     main()
